85ab2718ef4cfab71728bb5a9b88e643baf590d2
Daniil Markov <alisasuperdetka.kqecn@dralias.com>
2022-06-18 20:46:06 +0800

    Practice with objects

---
+++ getFullNames/index.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Object[]} users
+ */
+ const getFullNames = users => {
+	return users.map(user => {
+        return user.firstName.toUpperCase() + " " + user.lastName.toUpperCase();
+    });
+}
+
+// Sample usage - do not modify
+const users = [{
+  id: 1,
+  firstName: "Sam",
+  lastName: "Green"
+}, {
+  id: 2,
+  firstName: "Alex",
+  lastName: "Blue"
+}];
+
+console.log(getFullNames(users)); // ["SAM GREEN", "ALEX BLUE"]
