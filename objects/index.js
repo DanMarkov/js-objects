ebc2fc39d5322acf18095498fe770f3a6d51eb1f
Daniil Markov <alisasuperdetka.kqecn@dralias.com>
2022-06-18 20:47:17 +0800

    Practice with objects

--- objects/index.js
+++ objects/index.js
@@ -1,10 +1,26 @@
-const getGreeting = () => {
-    return "somebody";
-}
+const tweets = [
+    {
+      id: 10512,
+      message: "Hello Twitter 👋",
+      stats: {
+        likes: 41,
+        retweets: 54
+      }
+    },
+    {
+      id: 41241,
+      message: "How do you keep track of your notes?",
+      stats: {
+        likes: 14,
+        retweets: 20
+      }
+    }
+  ];
 
-const sayHello = name => {
-    return `Hello, ${name ?? getGreeting()}`;
-}
+  tweets.forEach(tweet => {
+    const {likes, retweets} = tweet.stats;
+    console.log(likes, retweets);
+  });
 
-console.log(sayHello("Papyrus"));
-console.log(sayHello());
+
+  console.log(1+NaN);
