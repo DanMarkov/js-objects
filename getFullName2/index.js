27a244ad597c59e95d8e35148d6aa6dddcacfd13
Daniil Markov <alisasuperdetka.kqecn@dralias.com>
2022-06-18 20:45:49 +0800

    Practice with ?? operator

--- getFullName2/index.js
+++ getFullName2/index.js
@@ -3,11 +3,11 @@
  * @param {object} [user.info]
  * @param {last} [user.info.name]
  */
- const getFullName = user => {
-	return user.info?.name ?? "N/A";  
-}
+const getFullName = (user) => {
+  return user.info?.name ?? 'N/A';
+};
 
 // Sample usage - do not modify
-console.log(getFullName({info: {name: "Sam"}})); // "Sam"
-console.log(getFullName({info: null})); // "N/A"
+console.log(getFullName({ info: { name: 'Sam' } })); // "Sam"
+console.log(getFullName({ info: null })); // "N/A"
 console.log(getFullName({})); // "N/A"
